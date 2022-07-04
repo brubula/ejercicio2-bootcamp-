7aeee61f70082c39f2238f1942b5671ca234516d
Kauz <brubula90@gmail.com>
2022-07-04 14:23:38 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const libro_favorito ={
+    titulo:"Harry Potter",
+    autor:"J.K. Rowling",
+    fecha:"June 21 1997",
+    url:"www.harrypotter.com",
+}
+
+const lista=["Bruno Bustamante",21,true,"October 22 2000",libro_favorito];
+
+
+console.log(lista);
+
+
+
+
